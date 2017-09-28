06039bffb2a5c08fa2896223b71810b193ce6fcc
mycamach <manoli.camacho.a@gmail.com>
2017-09-27 23:24:11 -0300

    updated changes (incidentally, I was working on another file)

--- ejercicio2.rb
+++ ejercicio2.rb
@@ -1,27 +1,33 @@
 module Formula
-  attr_accessor :lado
-# mixins para hacer composiciones
-# si se pasan con include a un método hijo, quedan como métodos de instancia
+  # se usan  para hacer composiciones
 
   def perimeter
-
-    if class.is_a? Cuadrado
-      @lado * 4
-    else 
-      puts hola
+    case self
+    # este case equivale a self.class (pero se pone solo con self en Ruby)
+    when Rectangulo
+      perimeter = 2 * (@largo + @ancho)
+      "El perímetro del rectángulo es #{perimeter}.\n"
+    when Cuadrado
+      perimeter = @lado * 4
+      "El perímetro del cuadrado es #{perimeter}.\n"
     end
+  end
 
   def area
-    if class.is_a? Cuadrado
-    @lado * @lado
-  else 
-    puts hola
+    case self
+    when Rectangulo
+      area = @largo * @ancho
+      "El área del rectángulo es #{area}.\n"
+    
+    when Cuadrado
+      area = @lado * @lado
+      "El área del cuadrado es #{area}.\n"
+    end
   end
-end
-end
 end
 
 class Rectangulo
+  attr_reader :largo, :ancho
   include Formula
 
   def initialize(largo, ancho)
@@ -30,11 +36,12 @@ class Rectangulo
   end
 
   def sides
-    print "El rectángulo tiene largo #{@largo} y ancho #{@ancho}.", "\n"
+    print "El rectángulo tiene largo #{@largo} y ancho #{@ancho}.\n"
   end
 end
 
 class Cuadrado
+  attr_reader :lado
   include Formula
 
   def initialize(lado)
@@ -42,9 +49,19 @@ class Cuadrado
   end
 
   def sides
-    print "El cuadrado tiene lados de medida #{@lado}.", "\n"
+    print "El cuadrado tiene lados de medida #{@lado}.\n"
   end
 end
 
 c1 = Cuadrado.new(2)
-puts c1.perimeter
+print c1.sides
+print c1.perimeter
+print c1.area
+
+r1 = Rectangulo.new(10, 4)
+print r1.sides
+print r1.perimeter
+print r1.area
+=begin cuando c1 llama a perimeter, el self del método devuelve
+la clase de c1 (o sea, Cuadrado)
+=end
